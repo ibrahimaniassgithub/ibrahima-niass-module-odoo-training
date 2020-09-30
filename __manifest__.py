94ce44f2331d092dfe1a5618882e1695150dd4ea
ibrahima niass <ibrahimaniassbb@gmail.com>
2020-09-30 14:21:38 +0000

    Création de la structure du module

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MY Academy",
+    'summary': "Manage trainings",
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+    """,
+    'author': "Ibrahima NIASS",
+    'category': 'Test_Technique',
+    'version': '1.0',
+    'depends': ['crm'],
+    'installable': True,
+    'application': True,
+    'demo': [ ],
+    'data': [
+        'views/teacher.xml',
+        'views/student.xml',
+        'views/course.xml',
+        'views/session.xml',
+        'views/theme.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml'
+    ],
+}
